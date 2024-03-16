e7518f00ebf7be3017751b61dcf92df91d6d1385
KanchanCS <kanchan111yadav@gmail.com>
2024-03-16 15:57:04 +0530

    palindrome_number

---
+++ palindrome_num.py
@@ -0,0 +1,22 @@
+#Palindrome program
+# A palindrome is a word, phrase, number, or other sequence of characters that reads the same forward and backward, ignoring spaces, punctuation, and capitalization.
+# Examples of palindromic words:
+# "radar"
+# "level"
+# "deified"
+# "civic"
+
+def is_palindrome(s):
+    s = ''.join(char.lower() for char in s if char.isalnum())
+    return s == s[::-1]
+
+word = input("Enter a word:")
+if is_palindrome(word):
+    print("palindrome")
+else:
+    print("not paildrome")    
+    
+    
+    
+
+
