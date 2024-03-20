546e3f2a21d6ef801148aff989af00cdccc799f3
KanchanCS <kanchan111yadav@gmail.com>
2024-03-20 17:33:55 +0530

    Average of Two Numbers.

---
+++ average-of-two-numbers.py
@@ -0,0 +1,9 @@
+#Average of Two Numbers.
+def average_of_two_numbers(num1, num2):
+    return (num1 + num2) / 2
+
+# Example usage:
+number1 = 10
+number2 = 20
+average = average_of_two_numbers(number1, number2)
+print(f"The average of {number1} and {number2} is: {average}")
