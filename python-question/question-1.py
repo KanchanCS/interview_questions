ce8987c7c9d7d3aa843aa4ba2e8766a31d1721c7
Kanchan Yadav <61403194+KanchanCS@users.noreply.github.com>
2024-03-31 09:03:46 +0530

    Update question-1.py

--- python-question/question-1.py
+++ python-question/question-1.py
@@ -1,7 +1,7 @@
 #Question 1: Find the Largest Number Write a Python function to find the largest number in a list of integers.
 #The function should take a list of numbers as input and return the largest number.
-###Input: [10, 20, 4, 45, 99]
-###Output: 99
+# Input: [10, 20, 4, 45, 99]
+# Output: 99
 
 
 def largest_num(numbers):
